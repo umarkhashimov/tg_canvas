7773631761ad4ef4b9d9d52bd949206eaf65535d
Umar Hoshimov <umarkashimov05@gmail.com>
2024-12-09 23:45:02 +0500

    fix

--- local_settings_example.py
+++ local_settings_example.py
@@ -5,7 +5,7 @@ TOKEN = ''
 # Default Admins List
 # {"name": "fullname", "tgid": "Telegram ID"} 
 DEFAULT_ADMINS= [
-    {}
+    {"name": "", "tgid": ""}
 ]
 
 
@@ -14,3 +14,12 @@ DEFAULT_ADMINS= [
 # database MongoDB
 db_connection = 'mongodb://localhost:27017/'
 db_name = 'tg_canvas'
+
+
+
+# REQUIRED DATABASE TABLES
+# admins
+# file_metadata
+# students
+# submissions
+# assignments
